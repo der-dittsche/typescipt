c005816463d5142d356c078d6d974865a5b06bb3
Sascha Dietrich <sascha_dietrich@outlook.com>
2022-10-29 16:05:28 +0200

    feat: finish map app

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -136883,12 +136883,16 @@ exports.User = void 0;
 var faker_1 = __importDefault(require("faker"));
 var User = /** @class */function () {
   function User() {
+    this.color = "green";
     this.name = faker_1.default.name.firstName();
     this.location = {
       lat: parseFloat(faker_1.default.address.latitude()),
       lng: parseFloat(faker_1.default.address.longitude())
     };
   }
+  User.prototype.markerContent = function () {
+    return "User Name: ".concat(this.name);
+  };
   return User;
 }();
 exports.User = User;
@@ -136907,6 +136911,7 @@ exports.Company = void 0;
 var faker_1 = __importDefault(require("faker"));
 var Company = /** @class */function () {
   function Company() {
+    this.color = "orange";
     this.companyName = faker_1.default.company.companyName();
     this.catchPhrase = faker_1.default.company.catchPhrase();
     this.location = {
@@ -136914,10 +136919,49 @@ var Company = /** @class */function () {
       lng: parseFloat(faker_1.default.address.longitude())
     };
   }
+  Company.prototype.markerContent = function () {
+    return "\n      <div>\n        <h1>Company Name: ".concat(this.companyName, "</h1> \n        <p>Catchphrase: ").concat(this.catchPhrase, "</p> \n      </div>\n    ");
+  };
   return Company;
 }();
 exports.Company = Company;
-},{"faker":"../node_modules/faker/index.js"}],"src/index.ts":[function(require,module,exports) {
+},{"faker":"../node_modules/faker/index.js"}],"src/CustomMap.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.CustomMap = void 0;
+var CustomMap = /** @class */function () {
+  function CustomMap(divId) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+  CustomMap.prototype.addMarker = function (mappable) {
+    var _this = this;
+    var marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+    var infoWindow = new google.maps.InfoWindow({
+      content: mappable.markerContent()
+    });
+    marker.addListener("click", function () {
+      infoWindow.open(_this.googleMap, marker);
+    });
+  };
+  return CustomMap;
+}();
+exports.CustomMap = CustomMap;
+},{}],"src/index.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -136925,11 +136969,13 @@ Object.defineProperty(exports, "__esModule", {
 });
 var User_1 = require("./User");
 var Company_1 = require("./Company");
+var CustomMap_1 = require("./CustomMap");
 var user = new User_1.User();
 var company = new Company_1.Company();
-console.log(user);
-console.log(company);
-},{"./User":"src/User.ts","./Company":"src/Company.ts"}],"C:/Users/Sascha Dietrich/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var customMap = new CustomMap_1.CustomMap("gmap");
+customMap.addMarker(user);
+customMap.addMarker(company);
+},{"./User":"src/User.ts","./Company":"src/Company.ts","./CustomMap":"src/CustomMap.ts"}],"C:/Users/Sascha Dietrich/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -136954,7 +137000,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64129" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58877" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
